795b1e749362849e30c65dd0ba7a4b0462ab66ac
Sebastian Köln <sebk@rynx.org>
2017-01-23 20:00:19 +0100

    add const initializers

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 #![no_std]
+#![feature(const_fn)]
 
 use core::ops::*;
 use core::fmt::{Formatter, Display, Debug, Error};
 
 const FRACTION_BITS: u8 = 8;
-const FRACTION_MASK: u32 = 0xFF;
+const FRACTION_MASK: i32 = 0xFF;
+const FRACTION_VALUE: i32 = 256;
+const SIGN_MASK: u32 = 0x8000_0000;
 const INT_BITS: u8 = 24;
 const BIT_VALUE: f64 = 0.00390625;
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct F24P8 {
-    bits: u32
+    bits: i32
 }
 impl Add for F24P8 {
     type Output = F24P8;
@@ -27,7 +30,19 @@ impl Sub for F24P8 {
 impl Mul for F24P8 {
     type Output = F24P8;
     fn mul(self, rhs: Self) -> Self {
-        F24P8 { bits: (self.bits * rhs.bits) >> FRACTION_BITS }
+        F24P8 { bits: (self.bits * rhs.bits) / FRACTION_VALUE }
+    }
+}
+impl Mul<i32> for F24P8 {
+    type Output = F24P8;
+    fn mul(self, rhs: i32) -> Self {
+        F24P8 { bits: self.bits * rhs }
+    }
+}
+impl Div<i32> for F24P8 {
+    type Output = F24P8;
+    fn div(self, rhs: i32) -> Self {
+        F24P8 { bits: self.bits / rhs }
     }
 }
 impl Display for F24P8 {
@@ -49,7 +64,12 @@ impl Debug for F24P8 {
 impl From<f64> for F24P8 {
     fn from(f: f64) -> F24P8 {
         assert!(f >= 0.);
-        F24P8 { bits: (f * (1<<FRACTION_BITS) as f64) as u32 }
+        F24P8 { bits: (f * FRACTION_VALUE as f64) as i32 }
+    }
+}
+impl From<i32> for F24P8 {
+    fn from(i: i32) -> F24P8 {
+        F24P8 { bits: i * FRACTION_VALUE }
     }
 }
 impl From<F24P8> for f64 {
@@ -59,9 +79,15 @@ impl From<F24P8> for f64 {
 }
 impl F24P8 {
     pub fn twice(self) -> F24P8 {
-        F24P8 { bits: self.bits << 1 }
+        F24P8 { bits: self.bits * 2 }
     }
     pub fn half(self) -> F24P8 {
-        F24P8 { bits: self.bits >> 1 }
+        F24P8 { bits: self.bits / 2 }
+    }
+    pub const fn from_int(i: i32) -> F24P8 {
+        F24P8 { bits: i * FRACTION_VALUE }
+    }
+    pub const fn from_float(f: f64) -> F24P8 {
+        F24P8 { bits: (f * FRACTION_VALUE as f64) as i32 }
     }
 }
--- tests/01.rs
+++ tests/01.rs
@@ -1,14 +1,35 @@
+#![feature(const_fn)]
+
 extern crate fp;
 
 use fp::F24P8;
+
+const TWO: F24P8 = F24P8::from_int(2);
+const PI: F24P8 = F24P8::from_float(::std::f64::consts::PI);
 
 #[test]
 fn add() {
     let fp_2 = F24P8::from(2.0);
     let fp_3 = F24P8::from(3.0);
+    assert_eq!(fp_2 + fp_2.half(), fp_3);
+}#[test]
+fn sub() {
+    let fp_2 = F24P8::from(2.0);
+    let fp_3 = F24P8::from(3.0);
+    assert_eq!(fp_3 - fp_2.half(), fp_2);
+}
+#[test]
+fn mul() {
+    let fp_2 = F24P8::from(2.0);
+    let fp_3 = F24P8::from(3.0);
+    assert_eq!(fp_2 * fp_3, fp_3 * fp_2);
+    assert_eq!(fp_2 * fp_3.half(), fp_3);
+}
+#[test]
+fn print() {
+    let fp_2 = F24P8::from(2.0);
+    let fp_3 = F24P8::from(3.0);
     println!("{} + {} = {}", fp_2, fp_3, fp_2 + fp_3);
     println!("{} - {} = {}", fp_3, fp_2, fp_3 - fp_2);
     println!("{} * {} = {}", fp_2, fp_3, fp_2 * fp_3);
-    
-    assert_eq!(fp_2 + fp_2.half(), fp_3);
 }
