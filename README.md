6dcff43a57b1cab04ed5520e10dd6135a3f9cea7
cbreeden <github@u.breeden.cc>
2017-01-23 21:15:47 -0600

    add more features

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Fixed Point Types
 
 Currently only implements F24P8. (i32 / 256).
-Supported operations are Add, Sub and Mul.
-Conversions from i32 -> F24P8 and f64 -> F24P8 -> f64 are implemented.
+Supported operations are Add, AddAssign, Sub, SubAssign, and Mul.
+Conversions from (u8, i8, u16, i16, u32, i32) -> F24P8 and f64 -> F24P8 -> f64 are implemented.
+
+Scalar multiplication: Int * F24P8 and F24P8 / Int.
--- src/lib.rs
+++ src/lib.rs
@@ -15,36 +15,89 @@ const BIT_VALUE: f64 = 0.00390625;
 pub struct F24P8 {
     bits: i32
 }
+
 impl Add for F24P8 {
     type Output = F24P8;
     fn add(self, rhs: Self) -> Self {
         F24P8 { bits: self.bits + rhs.bits }
     }
 }
+
+impl AddAssign for F24P8 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.bits = self.bits + rhs.bits
+    }
+}
+
 impl Sub for F24P8 {
     type Output = F24P8;
     fn sub(self, rhs: Self) -> Self {
         F24P8 { bits: self.bits - rhs.bits }
     }
 }
+
+impl SubAssign for F24P8 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.bits = self.bits - rhs.bits
+    }
+}
+
 impl Mul for F24P8 {
     type Output = F24P8;
     fn mul(self, rhs: Self) -> Self {
         F24P8 { bits: (self.bits * rhs.bits) / FRACTION_VALUE }
     }
 }
-impl Mul<i32> for F24P8 {
-    type Output = F24P8;
-    fn mul(self, rhs: i32) -> Self {
-        F24P8 { bits: self.bits * rhs }
+
+macro_rules! impl_fixed_muldiv {
+    ($($ty:ty),*) => (
+        $(
+        impl Mul<F24P8> for $ty {
+            type Output = F24P8;
+            fn mul(self, rhs: F24P8) -> Self::Output {
+                F24P8 { bits: (self as i32) * rhs.bits }
+            }
+        }
+        
+        impl Div<$ty> for F24P8 {
+            type Output = F24P8;
+            fn div(self, rhs: $ty) -> Self::Output {
+                F24P8 { bits: self.bits / (rhs as i32) }
+            }
+        }
+        )*
+    )
+}
+
+impl_fixed_muldiv! ( u8, i8, u16, i16, u32, i32 );
+
+macro_rules! impl_fixed_conv {
+    ($($ty:ty),*) => (
+        $(
+        impl From<$ty> for F24P8 {
+            fn from(n: $ty) -> F24P8 {
+                F24P8 { bits: (n as i32) * FRACTION_VALUE }
+            }
+        }
+        )*
+    )
+}
+
+impl_fixed_conv! ( u8, i8, u16, i16, u32, i32 );
+
+impl From<f64> for F24P8 {
+    fn from(f: f64) -> F24P8 {
+        assert!(f >= 0.);
+        F24P8 { bits: (f * FRACTION_VALUE as f64) as i32 }
     }
 }
-impl Div<i32> for F24P8 {
-    type Output = F24P8;
-    fn div(self, rhs: i32) -> Self {
-        F24P8 { bits: self.bits / rhs }
+
+impl From<F24P8> for f64 {
+    fn from(fp: F24P8) -> f64 {
+        fp.bits as f64 * BIT_VALUE
     }
 }
+
 impl Display for F24P8 {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
         write!(f, "{}.{:08}",
@@ -61,22 +114,7 @@ impl Debug for F24P8 {
         )
     }
 }
-impl From<f64> for F24P8 {
-    fn from(f: f64) -> F24P8 {
-        assert!(f >= 0.);
-        F24P8 { bits: (f * FRACTION_VALUE as f64) as i32 }
-    }
-}
-impl From<i32> for F24P8 {
-    fn from(i: i32) -> F24P8 {
-        F24P8 { bits: i * FRACTION_VALUE }
-    }
-}
-impl From<F24P8> for f64 {
-    fn from(fp: F24P8) -> f64 {
-        fp.bits as f64 * BIT_VALUE
-    }
-}
+
 impl F24P8 {
     pub fn twice(self) -> F24P8 {
         F24P8 { bits: self.bits * 2 }
